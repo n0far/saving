c759cbe77debbc2fb32aee258b8be791620d3f10
nofar <nofarbff@gmail.com>
2023-08-09 11:35:34 +0300

    first

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.cs'
+Vue.config.productionTip = false
+createApp(App).mount('#app')
